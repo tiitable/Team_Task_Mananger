02ff19a80f1c80eb4abd52e261c905dbacb2ae1a
tiitable <fklyc2@163.com>
2025-06-17 11:30:25 +0800

    アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,10 @@ module TeamTaskManager
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    # config.autoload_lib(ignore: %w[assets tasks])
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
 
     # Configuration for the application, engines, and railties goes here.
     #
